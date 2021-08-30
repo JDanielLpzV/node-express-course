48e38287a61b677cb8ecbd090fc4751181ae295d
JDanielLpzV <juandan.lopezvlqz.95@gmail.com>
2021-08-30 12:12:49 -0600

    Add dynamic GET route

--- server.js
+++ server.js
@@ -10,8 +10,17 @@ app.get('/users',function(req,res){
         success: true,
         message: 'successfully got users. Nice!',
         users: mockUserData
-    })
+    });
 });
+app.get('/users/:id',function(req,res){
+    console.log(req.params.id);
+    res.json({
+        success: true,
+        message:'got one user',
+        user: req.params.id
+    });
+});
+
 app.listen(8000,function(){
     console.log(("server is running"));
 })
